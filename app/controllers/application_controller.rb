27e2d4deabbeddd415d458e63ea5144f739fc8e3
Muaad <muadh24@gmail.com>
2018-03-14 01:58:51 +0300

    Route for poting to /

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,7 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :null_session
+
+  def tracker
+  	render json: {success: true}
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   resources :optimized_sites
   root to: 'pages#index'
-
+  post "/" => "application#tracker"
   get 'pages/string_test'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
