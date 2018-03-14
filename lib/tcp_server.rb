a288ccdf7a020b4355043cba18345c2853a093de
Muaad <muadh24@gmail.com>
2018-03-14 12:00:37 +0300

    Multi threaded tcp server and some logging

--- lib/tcp_server.rb
+++ lib/tcp_server.rb
@@ -1,9 +1,13 @@
 require 'socket'                 # Get sockets from stdlib
 
-server = TCPServer.open(2000)    # Socket to listen on port 2000
+server = TCPServer.open(2000)
+puts ">> Waiting for connections..."
 loop {                           # Servers run forever
-   client = server.accept        # Wait for a client to connect
-   client.puts(Time.now.ctime)   # Send the time to the client
-   client.puts "Closing the connection. Bye!"
-   client.close                  # Disconnect from the client
+   Thread.start(server.accept) do |client|
+   		puts ">> Connection received..."
+	   client.puts(Time.now.ctime)   # Send the time to the client
+	   client.puts('Hi. Thanks for connecting to me.')   # Send the time to the client
+	   client.puts "Closing the connection. Bye!"
+	   client.close                  # Disconnect from the client
+   end
 }
